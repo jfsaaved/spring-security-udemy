e9469c50e6ea4be8a8fd500347a189e938c88f3a
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2017-03-02 21:30:36 -0500

    First day for security Udemy

---
+++ src/main/java/com/jfsaaved/SpringSecurityUdemyApplication.java
@@ -0,0 +1,12 @@
+package com.jfsaaved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUdemyApplication.class, args);
+	}
+}
